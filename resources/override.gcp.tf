1ba180f030363245f3b1118a6c4d67bc6d463a0f
rimas <rmocius@gmail.com>
2018-07-06 18:04:44 +0100

    gcp public agent LB output fix

--- resources/override.gcp.tf
+++ resources/override.gcp.tf
@@ -6,7 +6,7 @@ output "Master Public IPs" {
   value = ["${google_compute_instance.master.*.network_interface.0.access_config.0.assigned_nat_ip}"]
 }
 
-output "Public Agent Load Balancer Address" {
+output "Public Agent Load Balancer Public IP" {
   value = "${google_compute_forwarding_rule.external-public-agent-forwarding-rule-http.ip_address}"
 }
 
